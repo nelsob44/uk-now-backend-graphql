cc44fa407af461fa5765d99eeb5e57ed1d839c0c
unknown <Nelson Oba>
2019-09-27 16:32:02 +0100

    first commit

--- controllers/email.js
+++ controllers/email.js
@@ -30,7 +30,7 @@ exports.sendEmail = async (req, res, next) => {
     try {
 
        await transporter.sendMail({
-            to: 'nelsondanbusiness@gmail.com',
+            to: 'payments@stridessolutions.com',
             from: senderEmail,
             subject: senderName +': UK Now user enquiry',
             html: messageDetail
