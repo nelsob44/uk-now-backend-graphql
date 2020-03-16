db0287805a837c2572ee9e519f251f1ce70d786e
unknown <Nelson Oba>
2020-03-16 20:54:30 +0000

    sendgrid change

--- controllers/email.js
+++ controllers/email.js
@@ -6,7 +6,7 @@ const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
-        api_key: ''
+        api_key: process.env.SENDGRID_KEY
     }
 }));
 
