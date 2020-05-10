512a100a93c0a3f9bb4fa48fb697e065010ea859
unknown <Nelson Oba>
2020-05-10 05:27:05 +0100

    cors changes

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.use(
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://uknow-591ad.firebaseapp.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if(req.method === 'OPTIONS') {
